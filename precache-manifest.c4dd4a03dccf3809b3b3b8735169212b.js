d4b00580a9a29ee6a0a85676cb9de059e8b5a1d2
RONAK JOSHI <51354961+rnkjoshi@users.noreply.github.com>
2020-07-24 20:58:59 +0530

    Updates

---
+++ precache-manifest.c4dd4a03dccf3809b3b3b8735169212b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48a7d71a5d83eb0b6377805f905beaec",
+    "url": "/trassshtalk/index.html"
+  },
+  {
+    "revision": "2e2ea71fea2930a5942f",
+    "url": "/trassshtalk/static/css/main.2ce28669.chunk.css"
+  },
+  {
+    "revision": "c935ff19983059132947",
+    "url": "/trassshtalk/static/js/2.156c44fb.chunk.js"
+  },
+  {
+    "revision": "df958f16b07871c3679510165e97a61a",
+    "url": "/trassshtalk/static/js/2.156c44fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e2ea71fea2930a5942f",
+    "url": "/trassshtalk/static/js/main.267fd9fb.chunk.js"
+  },
+  {
+    "revision": "66669dc54622ce73bf15",
+    "url": "/trassshtalk/static/js/runtime-main.23d9fb95.js"
+  },
+  {
+    "revision": "0765ba35fb0bcfd1e6d32fa8e1448e61",
+    "url": "/trassshtalk/static/media/download.0765ba35.png"
+  }
+]);
