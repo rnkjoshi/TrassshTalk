476bc8fe1ea08093b2766b5f10ebf2005272b32c
RONAK JOSHI <51354961+rnkjoshi@users.noreply.github.com>
2020-07-24 21:40:14 +0530

    msg

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import {BrowserRouter,Route} from 'react-router-dom'
+import {HashRouter,Route,Switch} from 'react-router-dom'
 import Join from './components/Join/Join'
 import Chat from './components/Chat/Chat'
 const App = () => {
     return (
-        <BrowserRouter>
+        <HashRouter>
+          <Switch>
             <Route path="/" exact component={Join} />
             <Route path="/chat" exact component={Chat} />
-        </BrowserRouter>
+          </Switch>
+        </HashRouter>
     );
 };
 
